b77a524c82fce3962a5538d27038559944846c60
LittleWhale <zverevn420@gmail.com>
2021-04-07 21:15:28 +0300

    Just return fetch

--- src/components/RegistrationPage/sendRegistrationRequest.ts
+++ src/components/RegistrationPage/sendRegistrationRequest.ts
@@ -1,22 +1,8 @@
 import {getMagicLinkUrl} from "../../config";
 
 
-export const sendRegistrationRequest = (email: string) => new Promise((resolve, reject) => {
-    fetch(getMagicLinkUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({email: email})
-    })
-        .then(response => {
-            if(response.status === 500){
-                reject(new Error('Invalid credentials'))
-            }
-            if(response.status === 400){
-                reject(new Error('Something goes wrong'))
-            }
-            resolve(response.status)
-        })
+export const sendRegistrationRequest = (email: string) => fetch(getMagicLinkUrl, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json;charset=utf-8'},
+    body: JSON.stringify({email: email})
 })
-

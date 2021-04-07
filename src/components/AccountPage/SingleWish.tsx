5bc8a10868c7d05f6c184745e9f7ef2242733ec8
LittleWhale <zverevn420@gmail.com>
2021-04-07 21:13:31 +0300

    Delete wish component implemented

--- src/components/AccountPage/SingleWish.tsx
+++ src/components/AccountPage/SingleWish.tsx
@@ -4,23 +4,32 @@ import FolderIcon from "@material-ui/icons/Folder";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
-import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import ListItem from "@material-ui/core/ListItem";
 import LockIcon from '@material-ui/icons/Lock';
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import React from "react";
+import {sendDeleteRequest} from "./sendDeleteRequest";
+import EditWishModal from "./EditWishModal";
 
 interface ISingleWishProps {
     wishTitle: string,
     wishDescription: string,
+    wishId: string,
     isPublic: boolean,
-    key: number
+    key: number,
+    onChange: () => void
 }
 
 class SingleWish extends React.Component<ISingleWishProps> {
+
+    deleteWish = () => {
+        sendDeleteRequest(this.props.wishId)
+            .catch((err: Error) => alert(err))
+    }
+
     render() {
-        const {wishTitle, wishDescription, isPublic} = this.props
+        const {wishTitle, wishDescription, wishId, isPublic} = this.props
         return (
             <ListItem>
                 <ListItemAvatar>
@@ -38,11 +47,16 @@ class SingleWish extends React.Component<ISingleWishProps> {
                 </ListItemIcon>
                 }
                 <ListItemSecondaryAction>
-                    <IconButton edge="start" aria-label="edit">
-                        <EditIcon/>
+                    <IconButton edge="end">
+                        <EditWishModal wishTitle={wishTitle} wishDescription={wishDescription} wishId={wishId}
+                                       onChange={() => this.props.onChange()}/>
                     </IconButton>
-                    <IconButton edge="end" aria-label="delete" >
-                        <DeleteIcon />
+                    <IconButton edge="end" aria-label="delete"
+                                onClick={async () => {
+                                    await this.deleteWish()
+                                    this.props.onChange()
+                                }}>
+                        <DeleteIcon/>
                     </IconButton>
                 </ListItemSecondaryAction>
             </ListItem>

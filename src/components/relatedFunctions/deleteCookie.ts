a706dd2fc154ad44eb3be2380166482a703f6498
LittleWhale <zverevn420@gmail.com>
2021-05-24 12:31:36 +0300

    text mask on token auth text field implemented

--- src/components/relatedFunctions/deleteCookie.ts
+++ src/components/relatedFunctions/deleteCookie.ts
@@ -1,5 +1,5 @@
 export const deleteCookie = () => {
     const cookieDate = new Date()
     cookieDate.setTime(cookieDate.getTime() - 1);
-    document.cookie = "auth-token=; domain=127.0.0.1; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT"
+    document.cookie = `auth-token=; domain=127.0.0.1; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT`
 }
